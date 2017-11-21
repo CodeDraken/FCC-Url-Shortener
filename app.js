66b84fe08931feb046c748f8b365597a4865895d
CodeDraken <codedraken@gmail.com>
2017-11-20 21:05:46 -0600

    use express router

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(express.static(`${__dirname}/public`))
 
-require('./routes/urlRoutes')(app)
+// routes
+app.use(require('./routes/urlRoutes'))
+
+// require('./routes/urlRoutes')(app)
 
 module.exports = app
--- routes/urlRoutes.js
+++ routes/urlRoutes.js
@@ -1,6 +1,13 @@
 const urlController = require('../controllers/urlController')
+const express = require('express')
+const urlRouter = express.Router()
 
-module.exports = app => {
+// use * to avoid issues with urls
+urlRouter.get('/new/:urlToShorten(*)?', urlController.newShortUrl)
+
+module.exports = urlRouter
+
+// module.exports = app => {
   // use * to avoid issues with urls
-  app.get('/new/:urlToShorten(*)?', urlController.newShortUrl)
-}
+  // app.get('/new/:urlToShorten(*)?', urlController.newShortUrl)
+// }
